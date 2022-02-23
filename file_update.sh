77e78588522befca865c29bc9eb3ab7105757ec2
socketflow <46785285+socketflow@users.noreply.github.com>
2022-02-23 14:57:45 +0000

    Create file_update.sh

---
+++ file_update.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -e
+
+# [title] this is a generic file updater script
+# [title] 这是一个通用性的文件更新脚本
+# [reference] 变量用 {} 的解释：https://stackoverflow.com/questions/8748831/when-do-we-need-curly-braces-around-shell-variables
+
+
+# [variable] 定义一些要用到的变量
+NEW_FILE_LINK='github_link'
+FILE_LOCAL_PATH="$HOME/dest_dir"
+FILE_LOCAL_NAME='app'
+FILE_PERMISSION='644'
+
+
+# 创建一个临时文件夹，如果创建失败则退出
+TMP_DIR=$(mktemp -d) || exit 1
+echo "temp dir created here: ${TMP_DIR}"
+
+
+# $1 是新版本的远程下载地址， $2 是本地文件名
+function download_newfile_to_tmp() {
+	if ! curl -L "${1}" -o "${TMP_DIR}/${2}"; then
+		echo 'ERROR: Download failed! Please check your network or try again.'
+		exit 1
+	fi
+}
+
+# fucntion download_checksum_to_tmp() {
+#   some code here
+# }
+
+# fucntion checksum() {
+#   some code here
+# }
+
+# $1是权限，$2是下载到临时文件的新文件，$3是目标path
+function install_tmpfile() {
+  install -m "$1" "${TMP_DIR}/${2}" "${3}"
+}
+
+
+# 定义主流程
+function main() {
+  # 先下载文件
+  download_newfile_to_tmp "${NEW_FILE_LINK}" "${FILE_LOCAL_NAME}"
+  echo "${NEW_FILE_LINK} is downloaded to ${TMP_DIR}/${FILE_LOCAL_NAME}"
+
+  # checksum
+
+  # 安装文件到固定位置（权限：755包含执行、644不包含执行）
+  install_tmpfile "${FILE_PERMISSION}" "${FILE_LOCAL_NAME}" "$FILE_LOCAL_PATH"
+  echo "${TMP_DIR}/${FILE_LOCAL_NAME} is installed to ${FILE_LOCAL_PATH}"
+
+  # 删除临时文件夹
+  rm -r -f "$TMP_DIR" || exit 1
+  echo "temp file deleted"
+}
+
+
+# 执行主流程
+main "$@"
