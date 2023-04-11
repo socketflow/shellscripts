713d51cdf596ec3f0c11eb318d921d904b563554
socketflow <46785285+socketflow@users.noreply.github.com>
2023-04-11 14:53:01 +0800

    Update file_update.sh
    format and comments tweaks

--- file_update.sh
+++ file_update.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-# [version] 20230411
+# [version] 20230411-2
 
 # [title] this is a generic file updater script
 # [title] 这是一个通用性的文件更新脚本
 # [reference] 变量用 {} 的解释：https://stackoverflow.com/questions/8748831/when-do-we-need-curly-braces-around-shell-variables
 
 
-# 1 [variable] 定义一些要用到的变量
+## 1 ROUTING FILE DOWNLOAD LINK
 
-# geoip.dat, for xray, for linux, from loyalsoldier | soffchen
+# 1.1 [geoip.dat]
+# for xray, from loyalsoldier | soffchen
 # soffchen的 cn ipv4 源换成了 [chnroutes2](https://github.com/misakaio/chnroutes2)
 if [ "${1}" = "geoip" ]; then
   FILE_LOCAL_NAME='geoip.dat'
   FILE_PERMISSION='644'
   # NEW_FILE_LINK='https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat'
+  # NEW_FILE_LINK='https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/release/geoip.dat'
   NEW_FILE_LINK='https://github.com/soffchen/geoip/releases/latest/download/geoip.dat'
 
   OS_NAME="$(uname)"
@@ -30,12 +32,14 @@ if [ "${1}" = "geoip" ]; then
   fi
 
 
-# geosite.dat, for xray, for linux, from loyalsoldier | MetaCubeX
+# 1.2 [geosite.dat] 
+# for xray, from loyalsoldier | MetaCubeX
 # MetaCubeX 的 geosite:cn 源换成了 [ChinaMax_Domain](https://github.com/blackmatrix7/ios_rule_script/tree/master/rule/Clash/ChinaMax)
 elif [ "${1}" = "geosite" ]; then
   FILE_LOCAL_NAME='geosite.dat'
   FILE_PERMISSION='644'
   # NEW_FILE_LINK='https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat'
+  # NEW_FILE_LINK='https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/release/geosite.dat'
   NEW_FILE_LINK='https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat'
 
   OS_NAME="$(uname)"
@@ -50,11 +54,13 @@ elif [ "${1}" = "geosite" ]; then
   fi
 
 
-# geoip.db, for sing-box, for linux, from soffchen
+# 1.3 [geoip.db]
+# for sing-box, from soffchen
 # soffchen的 cn ipv4 源换成了 [chnroutes2](https://github.com/misakaio/chnroutes2)
 elif [ "${1}" = "geoip-db" ]; then
   FILE_LOCAL_NAME='geoip.db'
   FILE_PERMISSION='644'
+  # NEW_FILE_LINK='https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/release/geoip.db'
   NEW_FILE_LINK='https://github.com/soffchen/sing-geoip/releases/latest/download/geoip.db'
 
   OS_NAME="$(uname)"
@@ -69,12 +75,14 @@ elif [ "${1}" = "geoip-db" ]; then
   fi
 
 
-# geosite.db, for sing-box, for linux, from soffchen | MetaCubeX
+# 1.4 [geosite.db]
+# for sing-box, from soffchen | MetaCubeX
 # MetaCubeX 的 geosite:cn 源换成了 [ChinaMax_Domain](https://github.com/blackmatrix7/ios_rule_script/tree/master/rule/Clash/ChinaMax)
 elif [ "${1}" = "geosite-db" ]; then
   FILE_LOCAL_NAME='geosite.db'
   FILE_PERMISSION='644'
   # NEW_FILE_LINK='https://github.com/soffchen/sing-geosite/releases/latest/download/geosite.db'
+  # NEW_FILE_LINK='https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/release/geosite.db'
   NEW_FILE_LINK='https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.db'
 
   OS_NAME="$(uname)"
@@ -89,7 +97,8 @@ elif [ "${1}" = "geosite-db" ]; then
   fi
 
 
-# geoip in mmdb format, for hysteria, for linux, from loyalsoldier
+# 1.5 [mmdb]
+# for hysteria, from loyalsoldier
 elif [ "${1}" = "mmdb" ]; then
   FILE_LOCAL_NAME='loyalsoldier.mmdb'
   FILE_PERMISSION='644'
@@ -107,25 +116,9 @@ elif [ "${1}" = "mmdb" ]; then
   fi
 
 
-# hysteria binary
-elif [ "${1}" = "hysteria" ]; then
-  FILE_LOCAL_NAME='hysteria'
-  FILE_PERMISSION='755'
-  FILE_LOCAL_PATH="/usr/local/bin"
+## 2 BINARY DOWNLOAD LINK
 
-  OS_NAME="$(uname)"
-
-  if [ "${OS_NAME}" = "Linux" ]; then
-    NEW_FILE_LINK='https://github.com/apernet/hysteria/releases/latest/download/hysteria-linux-amd64'
-  elif [ "${OS_NAME}" = "Darwin" ]; then
-    NEW_FILE_LINK='https://github.com/apernet/hysteria/releases/latest/download/hysteria-darwin-arm64'
-  else
-    NEW_FILE_LINK="error due to unknown operating system"
-    exit
-  fi
-  
-
-# xray-core binary, latest release
+# 2.1 [xray-core] bin, latest release
 elif [ "${1}" = "xray" ]; then
   FILE_LOCAL_NAME='xray.zip'
   FILE_PERMISSION='755'
@@ -143,7 +136,7 @@ elif [ "${1}" = "xray" ]; then
   fi
   
 
-# xray-core binary, user specified release
+# 2.2 [xray-core] bin, user specified release
 elif [ "${1}" = "xray-version" ]; then
   VERSION=v"${2}"
   FILE_LOCAL_NAME='xray.zip'
@@ -162,7 +155,7 @@ elif [ "${1}" = "xray-version" ]; then
   fi
 
 
-# sing-box binary, latest release
+# 2.3 [sing-box] bin, latest release
 elif [ "${1}" = "sing-box" ]; then
   FILE_LOCAL_NAME='sing-box.tar.gz'
   FILE_PERMISSION='755'
@@ -182,7 +175,7 @@ elif [ "${1}" = "sing-box" ]; then
   fi
 
 
-# sing-box binary, user specified release
+# 2.4 [sing-box] bin, user specified release
 elif [ "${1}" = "sing-box-version" ]; then
   VERSION="${2}"
   FILE_LOCAL_NAME='sing-box.tar.gz'
@@ -201,7 +194,7 @@ elif [ "${1}" = "sing-box-version" ]; then
   fi
 
 
-# warp-go binary, latest release, linux, amd64
+# 2.5 [warp-go] bin, latest release, linux, amd64
 elif [ "${1}" = "warp-go" ]; then
   FILE_LOCAL_NAME='warp-go.tar.gz'
   FILE_PERMISSION='755'
@@ -211,7 +204,25 @@ elif [ "${1}" = "warp-go" ]; then
   NEW_FILE_LINK="https://gitlab.com/ProjectWARP/warp-go/-/releases/${VERSION}/downloads/warp-go_${BIN_VERSION}_linux_amd64.tar.gz"
 
 
-# tuic binary, latest release, for linux *SERVER*, x86_64
+# 2.6 [hysteria] bin
+elif [ "${1}" = "hysteria" ]; then
+  FILE_LOCAL_NAME='hysteria'
+  FILE_PERMISSION='755'
+  FILE_LOCAL_PATH="/usr/local/bin"
+
+  OS_NAME="$(uname)"
+
+  if [ "${OS_NAME}" = "Linux" ]; then
+    NEW_FILE_LINK='https://github.com/apernet/hysteria/releases/latest/download/hysteria-linux-amd64'
+  elif [ "${OS_NAME}" = "Darwin" ]; then
+    NEW_FILE_LINK='https://github.com/apernet/hysteria/releases/latest/download/hysteria-darwin-arm64'
+  else
+    NEW_FILE_LINK="error due to unknown operating system"
+    exit
+  fi
+
+
+# 2.7 [tuic] bin, latest release
 elif [ "${1}" = "tuic" ]; then
   FILE_LOCAL_NAME='tuic'
   FILE_PERMISSION='755'
@@ -239,6 +250,10 @@ else
 
 fi
 
+
+## 3. FUNCTIONS
+
+# 3.1
 function echo_job() {
   echo ''
   echo ">> \$OS_NAME is $(uname)"
@@ -248,7 +263,8 @@ function echo_job() {
   echo ">> \$NEW_FILE_LINK is ${NEW_FILE_LINK}"
 }
 
-# 创建一个临时文件夹，如果创建失败则退出
+
+# 3.2 创建一个临时文件夹，如果创建失败则退出
 function make_tmpdir() {
   TMP_DIR=$(mktemp -d) || exit 1
   echo ''
@@ -256,6 +272,7 @@ function make_tmpdir() {
   echo ''
 }
 
+# 3.3 下载操作
 # $1 是新版本的远程下载地址， $2 是本地文件名
 function download_target_to_tmpdir() {
   if curl -L "${1}" -o "${TMP_DIR}/${2}"; then
@@ -269,16 +286,23 @@ function download_target_to_tmpdir() {
   fi
 }
 
+
+# 3.4 下载checksum，一直不会写
 # fucntion download_checksum_to_tmp() {
 #   some code here
 # }
 
+
+# 3.5 验证checksum，一直不会写
 # fucntion checksum() {
 #   some code here
 # }
 
+
+# 3.6 处理压缩文件
 # 如果下载的xray的zip文件，就需要先解压出binary文件（其他文件没用）
 function uncompress_tmpfile() {
+
   if [ -f ${TMP_DIR}/xray.zip ]; then
     echo ''
     echo ">> Zip file is found in ${TMP_DIR}"
@@ -289,6 +313,7 @@ function uncompress_tmpfile() {
     FILE_LOCAL_NAME='xray'
     echo ''
     echo ">> FILE_LOCAL_NAME changed to ${FILE_LOCAL_NAME}"
+
   elif [ -f ${TMP_DIR}/sing-box.tar.gz ]; then
     echo ''
     echo ">> tar.gz file is found in ${TMP_DIR}"
@@ -298,6 +323,7 @@ function uncompress_tmpfile() {
     FILE_LOCAL_NAME='sing-box'
     echo ''
     echo ">> FILE_LOCAL_NAME changed to ${FILE_LOCAL_NAME}"
+
   elif [ -f ${TMP_DIR}/warp-go.tar.gz ]; then
     echo ''
     echo ">> tar.gz file is found in ${TMP_DIR}"
@@ -307,12 +333,17 @@ function uncompress_tmpfile() {
     FILE_LOCAL_NAME='warp-go'
     echo ''
     echo ">> FILE_LOCAL_NAME changed to ${FILE_LOCAL_NAME}"
+
   else
     echo ''
     echo ">> No compressed file found. Continue to installation."
+
   fi
+
 }
 
+
+# 3.7 检查路径，没有的话就创建文件夹
 function check_install_path() {
   if [ -d "${FILE_LOCAL_PATH}" ]; then
     echo ''
@@ -325,6 +356,7 @@ function check_install_path() {
 }
 
 
+# 3.8 安装下载、解压好的文件
 # $1是权限，$2是下载到临时文件的新文件，$3是目标path
 function install_tmpfile() {
   if install -b -m "$1" "${TMP_DIR}/${2}" "${3}"; then
@@ -339,7 +371,8 @@ function install_tmpfile() {
 }
 
 
-# 清理临时文件，$1是脚本创建的临时文件夹地址
+# 3.9 清理临时文件夹
+# $1是脚本创建的临时文件夹地址
 function cleanup_tmpfile() {
   if rm -r -f "${TMP_DIR}"; then
     echo ''
@@ -354,7 +387,10 @@ function cleanup_tmpfile() {
 }
 
 
-# 定义主流程
+
+## 4. 主流程
+
+# 4.1 定义主流程
 function main() {
   echo_job
   make_tmpdir
@@ -367,5 +403,5 @@ function main() {
 }
 
 
-# 执行主流程
+# 4.2 执行主流程
 main "$@"
